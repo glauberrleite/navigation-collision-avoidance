f4d0d7e7ef02622b064b1f49bb5d7f470f5aac3f
glauberrleite <grl@ic.ufal.br>
2019-08-05 22:26:02 -0300

    OSQP test for mpc

---
+++ mpc_orca/src/MPC_ORCA.py
@@ -0,0 +1,103 @@
+import osqp
+import numpy
+import scipy.sparse as sparse
+
+class MPC_ORCA:
+
+    def __init__(self, goal, position, v_min, v_max, N, Ts):
+        """ MPC-ORCA controller instance
+        
+        :param goal: Goal position
+        :type goal: Numpy Array 2x1
+        :param position: Initial position
+        :type position: Numpy Array 2x1
+        :param v_min: Lower velocity constraint
+        :type v_min: float
+        :param v_max: Upper velocity constraint
+        :type v_max: float
+        :param N: Prediction Horizon
+        :type N: int
+        :param Ts: Sampling Time
+        :type Ts: float
+        :returns: Controller instance
+        :type: MPCORCA
+        """
+
+        self.N = N
+        self.Ts = Ts
+
+        # Linear Dynamics
+        # x = [p_x, p_y, v_x, v_y]'
+        # u = [a_x, a_y]'
+        Ad = sparse.csc_matrix([
+            [1.,    0., Ts, 0.  ],
+            [0.,    1., 0., Ts  ],
+            [0.,    0., 1., 0.  ],
+            [0.,    0., 0., 1.  ]
+        ])
+
+        Bd = sparse.csc_matrix([
+            [0.5 * Ts ** 2, 0.              ],
+            [0.,            0.5 * Ts ** 2   ],
+            [Ts,            0.              ],
+            [0.,            Ts              ]
+        ])
+        [self.nx, self.nu] = Bd.shape
+
+        # State constraints
+        xmin = numpy.array([-numpy.inf, -numpy.inf, v_min, v_min])
+        xmax = numpy.array([numpy.inf, numpy.inf, v_max, v_max])
+        umin = numpy.array([-numpy.inf, -numpy.inf])
+        umax = numpy.array([numpy.inf, numpy.inf])
+
+        # Initial state
+        x0 = numpy.array([position[0], position[1], 0., 0.])
+
+        # Setpoint
+        x_r = numpy.array([goal[0], goal[1], 0., 0.])
+
+        # MPC objective function
+        Q = sparse.diags([1., 1., 0., 0.])
+        Q_n = Q
+        R = sparse.eye(self.nu)
+
+        # Casting QP format
+        # QP objective
+        P = sparse.block_diag([sparse.kron(sparse.eye(N), Q), Q_n, sparse.kron(sparse.eye(N), R)]).tocsc()
+        q = numpy.hstack([numpy.kron(numpy.ones(N), -Q.dot(x_r)), -Q_n.dot(x_r), numpy.zeros(N * self.nu)])
+
+        # QP constraints
+        # - linear dynamics
+        Ax = sparse.kron(sparse.eye(N+1),-sparse.eye(self.nx)) + sparse.kron(sparse.eye(N+1, k=-1), Ad)
+        Bu = sparse.kron(sparse.vstack([sparse.csc_matrix((1, N)), sparse.eye(N)]), Bd)
+        Aeq = sparse.hstack([Ax, Bu])
+        leq = numpy.hstack([-x0, numpy.zeros(N*self.nx)])
+        ueq = leq
+
+        # - input and state constraints
+        Aineq = sparse.eye((N+1) * self.nx + N * self.nu)
+        self.lineq = numpy.hstack([numpy.kron(numpy.ones(N+1), xmin), numpy.kron(numpy.ones(N), umin)])
+        self.uineq = numpy.hstack([numpy.kron(numpy.ones(N+1), xmax), numpy.kron(numpy.ones(N), umax)])
+        
+        # - OSQP constraints
+        A = sparse.vstack([Aeq, Aineq]).tocsc()
+        l = numpy.hstack([leq, self.lineq])
+        u = numpy.hstack([ueq, self.uineq])
+
+        # Setting problem
+        self.problem = osqp.OSQP()
+        self.problem.setup(P, q, A, l, u, warm_start=True)
+
+    def getNewAcceleration(self, position, velocity):
+        x0 = numpy.array([position[0], position[1], velocity[0], velocity[1]])
+        leq = numpy.hstack([-x0, numpy.zeros(self.N * self.nx)])
+        ueq = leq
+
+        l_new = numpy.hstack([leq, self.lineq])
+        u_new = numpy.hstack([ueq, self.uineq])
+
+        self.problem.update(l=l_new, u=u_new)
+        result = self.problem.solve()
+
+        # return the first control input
+        return result.x[(-self.N*self.nu):(-(self.N-1)*self.nu)]
---
+++ mpc_orca/src/test_controller.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+
+import rospy
+import numpy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from MPC_ORCA import MPC_ORCA
+
+N_AGENTS = 4
+RADIUS = 0.7
+tau = 5
+
+Ts = 0.1
+X = [(-5, 0)]
+V = [(0, 0) for _ in xrange(len(X))]
+V_max = [1.0 for _ in xrange(len(X))]
+goal = [(5.0, 5.0)]
+
+
+def callback_0(msg):
+    X[0] = (float(msg.pose.pose.position.x), float(msg.pose.pose.position.y))
+    V[0] = (float(msg.twist.twist.linear.x), float(msg.twist.twist.linear.y))
+
+rospy.init_node('test_controller')
+
+sub_0 = rospy.Subscriber('/odom', Odometry, callback_0)
+pub_0 = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+t = 0
+
+controller = MPC_ORCA(goal[0], X[0], 0., V_max[0], 10, Ts)
+
+while not rospy.is_shutdown():
+    
+    a_new = controller.getNewAcceleration(X[0], V[0])
+    velocity = V[0] + a_new * Ts
+
+    vel_0 = Twist()
+    vel_0.linear.x = velocity[0]
+    vel_0.linear.y = velocity[1]
+
+    
+    pub_0.publish(vel_0)
+    
+    if t%100:
+        print(velocity)
+        #print(agents[0].velocity)
+    t += 1
+    rospy.sleep(Ts)
